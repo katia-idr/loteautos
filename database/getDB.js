08ff97ffd594af89589c96a7a4537a0c746c59cc
Eduardo Reveles <me@osiux.ws>
2023-09-07 15:53:49 -0600

    usar menos conexiones a la db

--- database/getDB.js
+++ database/getDB.js
@@ -1,27 +1,36 @@
-const mysql = require ('mysql2/promise');
-require ('dotenv').config();
+const mysql = require("mysql2/promise");
+require("dotenv").config();
 
-const {MYSQL_HOST, MYSQL_USER, MYSQL_PASS, MYSQL_DB, MYSQL_PORT, MYSQL_SPATH} = process.env;
+const {
+    MYSQL_HOST,
+    MYSQL_USER,
+    MYSQL_PASS,
+    MYSQL_DB,
+    MYSQL_PORT,
+    MYSQL_SPATH,
+} = process.env;
+
+let pool;
 
 const getDB = async () => {
-   let pool;
-   try {
-      if(!pool){
-         pool = mysql.createPool({
-            connectionLimit: 50,
-            host: MYSQL_HOST,
-            user: MYSQL_USER,
-            password: MYSQL_PASS,
-            database: MYSQL_DB,
-            port: MYSQL_PORT,
-            socketPath: MYSQL_SPATH,
-            timezone: 'Z',
-         })
-         return await pool.getConnection();
-      }  
-   } catch (error) {
-      console.error(error.message)
-   }
+    try {
+        if (!pool) {
+            pool = mysql.createPool({
+                connectionLimit: 50,
+                host: MYSQL_HOST,
+                user: MYSQL_USER,
+                password: MYSQL_PASS,
+                database: MYSQL_DB,
+                port: MYSQL_PORT,
+                socketPath: MYSQL_SPATH,
+                timezone: "Z",
+            });
+        }
+
+        return await pool.getConnection();
+    } catch (error) {
+        console.error(error.message);
+    }
 };
 
 module.exports = getDB;
